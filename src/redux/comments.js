2eba237b796155341daba83d540341c8ab313e8f
WardaFarhan <farhan.warda337@gmail.com>
2020-07-14 15:29:36 +0530

    Fetch from Server

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,3 +1,25 @@
+import * as ActionTypes from './ActionTypes';
+
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
+  switch (action.type) {
+    case ActionTypes.ADD_COMMENTS:
+      return {...state, errMess: null, comments: action.payload};
+
+    case ActionTypes.COMMENTS_FAILED:
+      return {...state, errMess: action.payload};
+
+    case ActionTypes.ADD_COMMENT:
+        var comment = action.payload;
+        comment.id = state.comments.length;
+        comment.date = new Date().toISOString();
+        return { ...state, comments: state.comments.concat(comment)};
+
+    default:
+      return state;
+  }
+};
+
+/*
 import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
@@ -14,3 +36,4 @@ export const Comments = (state = COMMENTS, action) => {
           return state;
       }
 };
+*/
